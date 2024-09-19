da85e837cac46d5ca9f5e189c5b3df3497cc9c74
Ann T Ropea <bedhanger@gmx.de>
2024-09-19 23:03:47 +0200

    wanipz.py: public_dnses: add some
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/wanipz.py
+++ python/wanipz.py
@@ -102,6 +102,18 @@ def naime():
                 'dns.opendns.com',
                 # Quad9
                 'dns11.quad9.net',
+                # TN-BB-AS Tunisia BackBone
+                'dns.defense.tn',
+                # Control D
+                'p0.freedns.controld.com',
+                # Cleanbrowsing
+                'security-filter-dns2.cleanbrowsing.org',
+                # OpenNIC
+                'paimon.sev.monster',
+                # CIRA Canadian Shield
+                'private.canadianshield.cira.ca',
+                # CenturyLink
+                'resolver2.lumen.tech',
             ]
             public_dns = random.choice(public_dnses)
 
