00c595234842485d336a5c9850b583a55954db6a
Ann T Ropea <bedhanger@gmx.de>
2024-12-08 00:20:06 +0100

    wanip.py: outsoure providers
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/wanip.py
+++ python/wanip.py
@@ -10,8 +10,9 @@ import sys
 from support.wanip import (
     curlme,
     parse_cmd_line,
+    providers,
 )
-def naime():
+def naime(providers):
     """
     Run the show
     """
@@ -20,24 +21,6 @@ def naime():
 
     if provider:
         providers = [provider]
-    else:
-        providers = [
-            'https://ifconfig.me/ip',
-            'https://my.ip.fi',
-            'https://icanhazip.com',
-            'https://ifconfig.co',
-            'https://ipecho.net/plain',
-            'https://ipinfo.io/ip',
-            'https://ident.me',
-            'https://ip.tyk.nu/',
-            'https://whatismyip.akamai.com',
-            'https://eth0.me/',
-            'https://api.ipify.org',
-            'https://ip.me',
-            'https://checkip.amazonaws.com',
-            'https://ipgrab.io',
-            'https://www.trackip.net/ip',
-        ]
     provider = random.choice(providers)
 
     if verbose > 1: print(providers)
@@ -46,4 +29,4 @@ def naime():
     curlme(provider=provider)
 
 if __name__ == '__main__':
-    sys.exit(naime())
+    sys.exit(naime(providers))
