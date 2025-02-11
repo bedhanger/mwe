950be31fec6a24487f83a36f656807b97439d74b
Ann T Ropea <bedhanger@gmx.de>
2025-02-11 21:25:02 +0100

    mwerunner.py: make docstrings PEP-compliant
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/runmwe/mwerunner.py
+++ python/support/runmwe/mwerunner.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python
-"""
-Base class for runners in MWE
-"""
+"""Base class for runners in MWE."""
 
 class MweRunner:
-    """
-    A runner from which others may be instantiated from
-    """
+    """A runner from which others may be instantiated from."""
+
     def __new__(cls, *pargs, **kwargs):
-        """
-        Make a runner
-        """
+        """Make a runner."""
         return super().__new__(cls)
 
     def __init__(self):
-        """
-        Init a newly made runner
-        """
+        """Init a newly made runner."""
 
     def __call__(self):
-        """
-        Allow the runner to be called
-        """
+        """Allow the runner to be called."""
 
     def __repr__(self):
-        """
-        Tell the world who we are, and where
-        """
+        """Tell the world who we are, and where."""
         return str(type(self)) + ' @ ' + hex(id(self))
