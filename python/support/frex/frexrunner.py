71188d66fcf8953224faf6f2ce1047ee6dcb9875
Ann T Ropea <bedhanger@gmx.de>
2025-03-02 19:31:19 +0100

    frequency.py: implement showing the overshoots
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/frex/frexrunner.py
+++ python/support/frex/frexrunner.py
@@ -49,11 +49,7 @@ class FrexRunner(MweRunner):
         self._logger.debug('Context established')
         return self
 
-    def __call__(self):
-        """Do the work."""
-        super().__call__()
-
-        self._logger.debug('Begining real work')
+    def _det_min_frex(self) -> float:
 
         self._logger.debug('Determining min frex')
         with open(self._min_frex_file) as _min_frex:
@@ -63,12 +59,62 @@ class FrexRunner(MweRunner):
         _minimum_freq = int(_minimum_freq) / 1000
         self._logger.debug('Minimum frequency is {mf}'.format(mf=_minimum_freq))
 
+        return _minimum_freq
+
+    def _det_frex_gov(self, of_cpu) -> str:
+
+        try:
+            _scaling_gov_file = Path(
+                '/sys/devices/system/cpu/cpu' + str(of_cpu)) / Path('cpufreq/scaling_governor')
+            assert _scaling_gov_file.exists()
+            with open(_scaling_gov_file) as _scaling_governor:
+                return _scaling_governor.readlines()[0]
+        except AssertionError:
+            self._logger.log(logging.ERROR, _scaling_gov_file, 'does not exist')
+
+    def _provide_overshoots(self, allowance, no_cpus):
+
+        self._logger.debug('Find fast CPUs et al.')
+        _how_many = 0
+        print('CPUs running faster than', allowance, 'MHz:')
+        with open(Path('/proc/cpuinfo')) as _cpuinfo:
+            for _line in _cpuinfo.readlines():
+                if 'processor' in _line:
+                    _core = _line
+                    _this_cpu = _core.split(':')[1].strip()
+                if 'model name' in _line:
+                    _model = _line
+                if 'cpu MHz' in _line:
+                    _mhz = float(_line.split(':')[1].lstrip())
+                    if _mhz > allowance:
+                        _how_many = _how_many + 1
+                        _frex_gov = self._det_frex_gov(_this_cpu)
+                        print(_core, _model, _line, 'frex governor   : ',_frex_gov, sep='', end='')
+
+        print('That is,', _how_many, 'out of', no_cpus)
+
+    def _provide_temp_readings(self):
+
+        print('Is cold')
+
+    def __call__(self):
+        """Do the work."""
+        super().__call__()
+
+        self._logger.debug('Begining real work')
+
+        _minimum_freq = self._det_min_frex()
+
         # 5 percent
         _margin = 1.05
         _allowance = _minimum_freq * _margin
 
         _no_cpus = subprocess.check_output('nproc').decode().rstrip()
         self._logger.debug('Found {n} CPUs'.format(n=_no_cpus))
+
+        self._provide_overshoots(_allowance, _no_cpus)
+
+        self._provide_temp_readings()
 
         self._logger.debug('Done')
 
