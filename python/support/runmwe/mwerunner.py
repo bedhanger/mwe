172fc3708540fc0e0ebaf75045010846e6577df2
Ann T Ropea <bedhanger@gmx.de>
2025-03-15 16:26:07 +0100

    mwerunner.py: refine diax
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/runmwe/mwerunner.py
+++ python/support/runmwe/mwerunner.py
@@ -34,7 +34,7 @@ class MweRunner:
 
     def __call__(self):
         """Allow the runner to be called if the context sentry allows it."""
-        assert self._ctx is not None, 'Context manager says: no context established'
+        assert self._ctx is not None, 'No context established.  Did you use a with-statement?'
 
     def __repr__(self):
         """Tell the world who we are, and where."""
