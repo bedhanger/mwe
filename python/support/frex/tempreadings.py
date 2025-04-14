e49c40c102b17987b90eb6aeb6d329f6776fbd09
Ann T Ropea <bedhanger@gmx.de>
2025-04-14 23:27:30 +0200

    tempreadings.py: refine
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/frex/tempreadings.py
+++ python/support/frex/tempreadings.py
@@ -4,27 +4,34 @@ import shutil
 import subprocess
 import re
 import textwrap
+import logging
 
 class TemperatureReadings:
 
     def __init__(self) -> None:
         self._sensors = shutil.which('sensors')
         self._tr = None
+        self._logger = logging.getLogger()
 
     def __enter__(self):
         try:
             assert self._sensors is not None
         except AssertionError as exc:
             self._logger.log(logging.ERROR, 'lm sensors package seems missing from system')
-            raise FileNotFoundError from exc
+            raise FileNotFoundError('Please install the lm sensors package') from exc
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
         return False
 
-    def __call__(self) -> None:
-        _sensors = subprocess.check_output(r'sensors').decode()
+    def __call__(self) -> iter:
+        """Inspect the sensors output to find temperature matches.
 
+        Care about values for CPU and motherboard.
+        Return them as an iterator.
+        """
+
+        _sensors = subprocess.check_output(self._sensors).decode()
         _temps_rx = re.compile(r'''
             (?P<selector>(CPU|MB))
             \s
@@ -34,8 +41,8 @@ class TemperatureReadings:
             \s+
             (?P<values>.+)
             ''', re.VERBOSE)
-
         self._tr = re.finditer(_temps_rx, _sensors)
+        return self._tr
 
     def __repr__(self) -> object:
         _me = type(self).__name__
@@ -45,16 +52,24 @@ class TemperatureReadings:
         try:
             assert self._tr is not None
         except AssertionError as exc:
-            return 'Temperature readings not avalable'
+            return 'Temperature readings not available'
+
+        # Unwind the iterator
         _pretty = ''
         for _s in self._tr:
             _pretty = _pretty + textwrap.dedent(f"""
-                The reading for {_s.group('selector')} is\t{_s.group('values')}
+                The temperature reading for the {_s.group('selector')} is\t{_s.group('values')}
             """).lstrip()
-        return _pretty
+        return _pretty.strip()
 
 if __name__ == '__main__':
 
     with TemperatureReadings() as tr:
-        _ = tr()
-        print(tr, end='')
+        print(tr)
+        temperature_readings = tr()
+        print(tr)
+
+        # You can iterate over the reading matches if you like...(need to rewind the iterator)
+        temperature_readings = tr()
+        for i in temperature_readings:
+            print(i)
