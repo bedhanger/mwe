7d0d279b9e7dbf2b6b1c0b6acba0bd2f1740bfc2
Ann T Ropea <bedhanger@gmx.de>
2025-04-26 19:59:21 +0200

    tempreadings.py: separate running the thing from traversing the results, customise more
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/frex/tempreadings.py
+++ python/support/frex/tempreadings.py
@@ -5,12 +5,14 @@ import subprocess
 import re
 import textwrap
 import logging
+import pytest
 
 class TemperatureReadings:
 
-    def __init__(self) -> None:
+    def __init__(self, auto_read=True) -> None:
         self._sensors = shutil.which('sensors')
         self._tr = None
+        self._auto_read = auto_read
         self._logger = logging.getLogger()
 
     def __enter__(self):
@@ -24,11 +26,10 @@ class TemperatureReadings:
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
         return False
 
-    def __call__(self) -> iter:
+    def __call__(self) -> None:
         """Inspect the sensors output to find temperature matches.
 
         Care about values for CPU and motherboard.
-        Return them as an iterator.
         """
 
         _sensors = subprocess.check_output(self._sensors).decode()
@@ -42,17 +43,24 @@ class TemperatureReadings:
             (?P<values>.+)
             ''', re.VERBOSE)
         self._tr = re.finditer(_temps_rx, _sensors)
+
+    def __iter__(self):
+        """Arm and return an iterator for the raw matches."""
+        if self._auto_read:
+            self.__call__()
         return self._tr
 
     def __repr__(self) -> object:
-        _me = type(self).__name__
+        _me = self.__class__.__name__
         return f"{_me}()"
 
     def __str__(self) -> str:
+        if self._auto_read:
+            self.__call__()
         try:
             assert self._tr is not None
         except AssertionError as exc:
-            return 'Temperature readings not available'
+            return 'Temperature readings are not (yet?) available'
 
         # Unwind the iterator
         _pretty = ''
@@ -65,11 +73,32 @@ class TemperatureReadings:
 if __name__ == '__main__':
 
     with TemperatureReadings() as tr:
-        print(tr)
-        temperature_readings = tr()
+
+        # Id ourselves
+        print(repr(tr))
+
+        # Pretty-print results
         print(tr)
 
-        # You can iterate over the reading matches if you like...(need to rewind the iterator)
-        temperature_readings = tr()
-        for i in temperature_readings:
+        # You can iterate over the raw reading matches if you like
+        for i in tr:
+            print(i)
+
+    # Now prepare everything, but don't actually read the values
+    with TemperatureReadings(auto_read=False) as tr:
+
+        # Id ourselves
+        print(repr(tr))
+
+        # "Pretty-print" results
+        print(tr)
+
+        # You cannot iterate over the raw reading matches unless you perform the reading yourself
+        with pytest.raises(TypeError):
+            for i in tr:
+                print(i)
+
+        # Once you do, you can
+        tr()
+        for i in tr:
             print(i)
