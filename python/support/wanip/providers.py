9621190e150c5456bb9b934ee1a2d070bce01e09
Ann T Ropea <bedhanger@gmx.de>
2025-05-16 17:20:06 +0200

    providers.py: refine a little and add test code
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/wanip/providers.py
+++ python/support/wanip/providers.py
@@ -25,15 +25,81 @@ from support.lsattr import LsAttr
 class Providers(LsAttr):
 
     def __init__(self, *pargs):
-        self.public_providers = tuple(pargs)
+        self.providers = tuple(pargs)
 
-    def __call__(self) -> str:
-        return random.choice(self.public_providers)
+    def __call__(self):
+        return random.choice(self.providers)
 
-Public_Providers = Providers(1, 2, 3, 4, 5, 6)
+    def __add__(self, another):
+        _ = list(self.providers)
+        _.append(another)
+        self.providers = tuple(_)
+        return self
+
+    def __sub__(self, one):
+        _ = list(self.providers)
+        _.remove(one)
+        self.providers = tuple(_)
+        return self
+
+    def __len__(self):
+        return len(self.providers)
+
+    def __contains__(self, provider):
+        return provider in self.providers
+
+Public_Providers = Providers(*public_providers)
 
 if __name__ ==  '__main__':
 
-    print(Public_Providers)
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('"my machine" is in Public_Providers? ', "my machine" in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
 
-    print(Public_Providers())
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('"https://ifconfig.me/ip" is in Public_Providers? ', "https://ifconfig.me/ip" in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Providers(1, 2, 3, 4, 5, 6)
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('6 is in Public_Providers? ', 6 in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers + 42
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('4 is in Public_Providers? ', 42 in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers + 'foo'
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('\'foo\' is in Public_Providers? ', 'foo' in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers + "bar"
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('"baz" is in Public_Providers? ', "baz" in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers + "zonk" + 'baz'
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('"baz" is in Public_Providers? ', "baz" in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers - "zonk" - 3
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('3 is in Public_Providers? ', 3 in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers(), end='\n\n')
+
+    Public_Providers = Public_Providers - 4
+    print('There are currently', len(Public_Providers), 'public providers')
+    print('4 is in Public_Providers? ', 4 in Public_Providers)
+    print('This is what Public_Providers is internally:', Public_Providers)
+    print('Here is a public provider:', Public_Providers())
