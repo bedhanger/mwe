a7e8f4b6e73d08c896fb625b648be174d208f122
Ann T Ropea <bedhanger@gmx.de>
2025-05-17 18:47:25 +0200

    providers.py: stop using public_providers in defining Public_Providers
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/wanip/providers.py
+++ python/support/wanip/providers.py
@@ -129,7 +129,25 @@ class Providers(LsAttr):
         return False
 
 # Export this
-Public_Providers = Providers(*public_providers)
+Public_Providers = Providers(
+    'https://ifconfig.me/ip',
+    'https://my.ip.fi',
+    'https://icanhazip.com',
+    'https://ifconfig.co',
+    'https://ipecho.net/plain',
+    'https://ipinfo.io/ip',
+    'https://ident.me',
+    'https://iprs.fly.dev',
+    'https://l2.io/ip',
+    'https://ipapi.co/ip',
+    'https://wgetip.com',
+    'https://whatismyip.akamai.com',
+    'https://eth0.me/',
+    'https://api.ipify.org',
+    'https://ip.me',
+    'https://checkip.amazonaws.com',
+    'https://www.trackip.net/ip',
+)
 
 if __name__ ==  '__main__':
     """Self test code"""
