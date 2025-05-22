1ad729c0f27c5005337035d0ff49bb78de47a4a7
Ann T Ropea <bedhanger@gmx.de>
2025-05-22 17:49:10 +0200

    wanip: turn more self-test code into unittest behaviour
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/wanip/__main__.py
+++ python/support/wanip/__main__.py
@@ -11,66 +11,42 @@ class ProvidersTestcase_01(unittest.TestCase):
 
         global Public_Providers
 
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('"my machine" is in Public_Providers? ', "my machine" in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 17
+        assert "my machine" not in Public_Providers
+        assert repr(Public_Providers) is not None
+        assert str(Public_Providers) is not None
+        P = Public_Providers()
+        assert P in Public_Providers
 
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('"https://ifconfig.me/ip" is in Public_Providers? ', "https://ifconfig.me/ip" in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert "https://ifconfig.me/ip" in Public_Providers
 
         Public_Providers = Providers(1, 2, 3, 4, 5, 6)
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('6 is in Public_Providers? ', 6 in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 6
+        assert "6" in Public_Providers
 
         Public_Providers = Public_Providers + 42
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('4 is in Public_Providers? ', 42 in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 7
+        assert "42" in Public_Providers
 
         Public_Providers = Public_Providers + 'foo'
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('\'foo\' is in Public_Providers? ', 'foo' in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 8
+        assert 'foo' in Public_Providers
 
         Public_Providers = Public_Providers + "bar"
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('"baz" is in Public_Providers? ', "baz" in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 9
+        assert "baz" not in Public_Providers
 
         Public_Providers = Public_Providers + "zonk" + 'baz'
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('"baz" is in Public_Providers? ', "baz" in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 11
+        assert "baz" in Public_Providers
 
         Public_Providers = Public_Providers - "zonk" - 3
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('3 is in Public_Providers? ', 3 in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 9
+        assert "3" not in Public_Providers
 
         Public_Providers = Public_Providers - 4
-        print('There are currently', len(Public_Providers), 'public providers')
-        print('4 is in Public_Providers? ', 4 in Public_Providers)
-        print('This is what Public_Providers is internally:', repr(Public_Providers))
-        print('The public providers:'); print(Public_Providers)
-        print('Here is a public provider:', Public_Providers(), end='\n\n')
+        assert len(Public_Providers) == 8
+        assert "4" not in Public_Providers
 
     def test_removing_an_unknown_element(self):
         # Cannot remove non-existing element
