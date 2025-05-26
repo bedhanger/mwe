fe57e6d053ba4b04004a5cd905309f9eb2da3e41
Ann T Ropea <bedhanger@gmx.de>
2025-05-26 21:38:23 +0200

    FastCpu: find solutions for the print tests in the unittest
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/support/frex/__main__.py
+++ python/support/frex/__main__.py
@@ -1,6 +1,7 @@
 """Unit tests"""
 
 import unittest
+import textwrap
 import pytest
 
 from . import FastCpu
@@ -12,17 +13,35 @@ class FastCpuTestCase(unittest.TestCase):
         X = FastCpu(42, 'Alpha', 300)
         Y = FastCpu(22, 'Vax', 30)
 
+        assert str(X).lstrip() == textwrap.dedent(f'''
+            Processor id  : 42
+            Model name    : Alpha
+            Speed in MHz  : 300
+            Frex governor : None
+        ''').lstrip()
+
+        assert str(Y).lstrip() == textwrap.dedent(f'''
+            Processor id  : 22
+            Model name    : Vax
+            Speed in MHz  : 30
+            Frex governor : None
+        ''').lstrip()
+
         Z = []
         Z.append(X)
         Z.append(Y)
+
         with FastCpu(identity=123.3, model='Turing Machine', mhz=1) as q:
             Z.append(q)
 
-        assert len(Z) == 3
-        print('-' * 100)
-        for p in Z:
-            print(p, end='-' * 100 + '\n')
+            assert len(Z) == 3
 
-        assert repr(q) == f"FastCpu(governor={hex(id(q))!r}, identity=123.3, mhz=1, model='Turing Machine')"
+            assert repr(q) == f"FastCpu(governor={hex(id(q))!r}, identity=123.3, mhz=1, model='Turing Machine')"
+            assert str(q).lstrip() == textwrap.dedent(f'''
+                Processor id  : 123.3
+                Model name    : Turing Machine
+                Speed in MHz  : 1
+                Frex governor : {hex(id(q))}
+            ''').lstrip()
 
 unittest.main(verbosity=3)
