950753e8e5857c5ccf2f2ccaf90d4f33a07a6e69
Ann T Ropea <bedhanger@gmx.de>
2025-05-29 01:25:35 +0200

    test_lsattr.py: get rid of unittest
    This is a temporary fix until we have figured out how to combine the three things
    
        * pytest for running the script
        * unittest AND pytest for running the tests
    
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/lsattr/tests/test_lsattr.py
+++ python/lsattr/tests/test_lsattr.py
@@ -1,55 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env --split-string=pytest --verbose
 
 """Self-tests"""
-
-import unittest
 import pytest
 
 from lsattr import LsAttr
 
-class LsAttrTestCase_01(unittest.TestCase):
+def test_general():
+    X = LsAttr()
+    assert repr(X) == 'LsAttr()'
 
-    def test_general(self):
-        X = LsAttr()
-        assert repr(X) == 'LsAttr()'
+def test_single_inheritance():
+    class X(LsAttr):
+        pass
+    assert repr(X()) == 'X()'
 
-    def test_single_inheritance(self):
-        class X(LsAttr):
-            pass
-        assert repr(X()) == 'X()'
+    class X(LsAttr):
+        def __init__(self):
+            self.x = 1
+    assert repr(X()) == 'X(x=1)'
 
-        class X(LsAttr):
-            def __init__(self):
-                self.x = 1
-        assert repr(X()) == 'X(x=1)'
+    class X(LsAttr):
+        def __init__(self):
+            self.x = 1
+            self.y = 2
+    assert repr(X()) == 'X(x=1, y=2)'
 
-        class X(LsAttr):
-            def __init__(self):
-                self.x = 1
-                self.y = 2
-        assert repr(X()) == 'X(x=1, y=2)'
+def test_multiple_inheritance():
+    class X(LsAttr, list):
+        pass
+    assert repr(X()) == 'X()'
 
-    def test_multiple_inheritance(self):
-        class X(LsAttr, list):
-            pass
-        assert repr(X()) == 'X()'
+    class X():
+        def __init__(self):
+            self.y = None
+    assert repr(X()) != 'X()' # !!!
 
-        class X():
-            def __init__(self):
-                self.y = None
-        assert repr(X()) != 'X()' # !!!
+    class Y(LsAttr, X):
+        def __init__(self):
+            super().__init__()
+            self.z = 'Hi'
+    assert repr(Y()) == "Y(y=None, z='Hi')"
 
-        class Y(LsAttr, X):
-            def __init__(self):
-                super().__init__()
-                self.z = 'Hi'
-        assert repr(Y()) == "Y(y=None, z='Hi')"
-
-    def test_defaults(self):
-        class X(LsAttr):
-            def __init__(self, a=[]):
-                self.a = a
-        assert repr(X()) == "X(a=[])"
-        assert repr(X(a=['b'])) == "X(a=['b'])"
-
-unittest.main(verbosity=3)
+def test_defaults():
+    class X(LsAttr):
+        def __init__(self, a=[]):
+            self.a = a
+    assert repr(X()) == "X(a=[])"
+    assert repr(X(a=['b'])) == "X(a=['b'])"
