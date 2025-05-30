8bce807571d3ba4d4ba8a38c342bf2b4a1cdc8c5
Ann T Ropea <bedhanger@gmx.de>
2025-05-30 14:50:12 +0200

    test_pathorstr.py: add a rudimentary test
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

---
+++ python/pathorstr/tests/test_pathorstr.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env --split-string=pytest --verbose
+
+import pytest
+from pathlib import Path
+from typing import Union, TypeAliasType
+
+from pathorstr import PathOrStr
+
+def test_general():
+    assert type(PathOrStr) is Union or TypeAliasType
