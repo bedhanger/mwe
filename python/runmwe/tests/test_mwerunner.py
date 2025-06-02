18ce568112559e2233e54d43d02f0f99be052e8b
Ann T Ropea <bedhanger@gmx.de>
2025-06-02 22:22:55 +0200

    test_mwerunner.py: use pytest fixtures to distinguish between 'good' and 'bad' runners
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/runmwe/tests/test_mwerunner.py
+++ python/runmwe/tests/test_mwerunner.py
@@ -3,30 +3,39 @@
 """Self-test code"""
 
 import pytest
+from typing import Iterator
 
 from runmwe import MweRunner
 from ooce.exceptions import OutOfContextError
 
+@pytest.fixture
+def contextless_runner() -> Iterator[MweRunner]:
+    R = MweRunner()
+    yield R
+
+@pytest.fixture
+def contextful_runner() -> Iterator[MweRunner]:
+    with MweRunner() as R:
+        yield R
+
 class TestCaseMweRunner_01:
 
-    def test_context_based_usage(self):
+    def test_context_based_usage(self, contextful_runner):
 
-        with MweRunner() as R:
-            R()
-            print(repr(R))
-            print(R)
+        contextful_runner()
+        print(repr(contextful_runner))
+        print(contextful_runner)
 
-    def test_contextless_usage(self):
-
-        R = MweRunner()
-        with pytest.raises(OutOfContextError):
-            R()
+    def test_contextless_usage(self, contextless_runner):
 
         with pytest.raises(OutOfContextError):
-            print(R)
+            contextless_runner()
 
-    def test_legal_contextless_usage(self):
+        with pytest.raises(OutOfContextError):
+            print(contextless_runner)
 
-        R = MweRunner()
-        # This can be called outwith a context
-        print(repr(R))
+    def test_legal_contextless_usage(self, contextless_runner, contextful_runner):
+
+        # This can be called outwith a context or within
+        print(repr(contextless_runner))
+        print(repr(contextful_runner))
