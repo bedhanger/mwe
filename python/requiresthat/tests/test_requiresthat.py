12b81bb0464ff20fae7907ac43f89d1b790424c1
Ann T Ropea <bedhanger@gmx.de>
2025-06-10 22:59:17 +0200

    requiresthat: refine tests
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/requiresthat/tests/test_requiresthat.py
+++ python/requiresthat/tests/test_requiresthat.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env --split-string=python -m pytest --verbose
 
-from requiresthat import requires
+import pytest
+
+from requiresthat import requires, RequirementNotFulfilledError
 
 class TestCase_requiresthat_01:
 
-    def test_general(self):
+    def test_good(self):
 
         class Spam:
             def __init__(self):
@@ -16,5 +18,35 @@ class TestCase_requiresthat_01:
             def run(self):
                 print('Running')
 
-            S = Spam()
+        S = Spam()
+        S.run()
+
+    def test_bad(self):
+
+        class Spam:
+            def __init__(self):
+                self.foo = 66
+                self.bar = ...
+
+            @requires(that='self.foo == 66')
+            @requires(that='self.bar is None')
+            def run(self):
+                print('Running')
+
+        S = Spam()
+        with pytest.raises(RequirementNotFulfilledError):
             S.run()
+
+    @pytest.mark.filterwarnings("ignore::SyntaxWarning")
+    def test_ugly(self):
+
+        class Spam:
+            def __init__(self):
+                pass
+
+            @requires(that=b"'24' is not 'the answer'")
+            def run(self):
+                print('Running')
+
+        S = Spam()
+        S.run()
