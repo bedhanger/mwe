62679a5bda9c9250c3d87471570b5b206e31c10e
Ann T Ropea <bedhanger@gmx.de>
2025-08-16 19:31:31 +0200

    filtermessages: carry over the first batch of regexes from the to-be-decommissioned Perl script
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/filtermessages/src/filtermessages/filtermessages.py
+++ python/filtermessages/src/filtermessages/filtermessages.py
@@ -11,17 +11,33 @@ from typing import Optional
 class FilterMessages:
 
     DEFAULT_THINGS_CONSIDERED_BORING : re.Pattern = re.compile(r'''
-        """
+        (?:b(ed)?h(anger)?\svdr:)
         |
-        __
+        (?:(dovecot)|(log)(\[\d+\])?:\simap)
         |
-        \#
+        (?:b(ed)?h(anger)?\sfirefox(\.desktop)?(\[\d+\])?:)
         |
-        :
+        (?:b(ed)?h(anger)?\smp3fs(\[\d+\])?:)
         |
-        \|
+        (?:b(ed)?h(anger)?\scron.*:)
         |
-        '
+        (?:failed\sto\scoldplug\sunifying.*:)
+        |
+        (?:b(ed)?h(anger)?\ssmartd.*:.*SMART\sUsage\sAttribute:\s194\sTemperature_Celsius\schanged\sfrom)
+        |
+        (?:EMITTING\sCHANGED\sfor)
+        |
+        (?:helper\(pid\s+\d+\):\scompleted\swith\sexit\scode\s0)
+        |
+        (?:helper\(pid\s+\d+\):\slaunched\sjob\sudisks-helper-ata-smart-collect\son)
+        |
+        (?:Refreshing\sATA\sSMART\sdata\sfor)
+        |
+        (?:dvb_frontend_get_frequency_limits)
+        |
+        (?:systemd\[1\]:\sCondition\scheck\sresulted\sin\s.+\sbeing\sskipped\.)
+        |
+        (?:gdm-x-session\[\d+\]:\s>\sWarning:\s+Could\snot\sresolve\skeysym\s\S+)
     ''', re.VERBOSE)
 
     def __init__(self,
