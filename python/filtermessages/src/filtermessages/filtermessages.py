9c5f52eed371926ab90d6eec47dea7b5c0d5dc49
Ann T Ropea <bedhanger@gmx.de>
2025-08-18 21:56:03 +0200

    filtermessages: outsource (and protect) DEFAULT_THINGS_CONSIDERED_BORING
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- python/filtermessages/src/filtermessages/filtermessages.py
+++ python/filtermessages/src/filtermessages/filtermessages.py
@@ -8,37 +8,9 @@ import argparse
 from pathlib import PurePath
 from typing import Optional
 
-class FilterMessages:
+from .burden import DEFAULT_THINGS_CONSIDERED_BORING
 
-    DEFAULT_THINGS_CONSIDERED_BORING : re.Pattern = re.compile(r'''
-        (?:b(ed)?h(anger)?\svdr:)
-        |
-        (?:(dovecot)|(log)(\[\d+\])?:\simap)
-        |
-        (?:b(ed)?h(anger)?\sfirefox(\.desktop)?(\[\d+\])?:)
-        |
-        (?:b(ed)?h(anger)?\smp3fs(\[\d+\])?:)
-        |
-        (?:b(ed)?h(anger)?\scron.*:)
-        |
-        (?:failed\sto\scoldplug\sunifying.*:)
-        |
-        (?:b(ed)?h(anger)?\ssmartd.*:.*SMART\sUsage\sAttribute:\s194\sTemperature_Celsius\schanged\sfrom)
-        |
-        (?:EMITTING\sCHANGED\sfor)
-        |
-        (?:helper\(pid\s+\d+\):\scompleted\swith\sexit\scode\s0)
-        |
-        (?:helper\(pid\s+\d+\):\slaunched\sjob\sudisks-helper-ata-smart-collect\son)
-        |
-        (?:Refreshing\sATA\sSMART\sdata\sfor)
-        |
-        (?:dvb_frontend_get_frequency_limits)
-        |
-        (?:systemd\[1\]:\sCondition\scheck\sresulted\sin\s.+\sbeing\sskipped\.)
-        |
-        (?:gdm-x-session\[\d+\]:\s>\sWarning:\s+Could\snot\sresolve\skeysym\s\S+)
-    ''', re.VERBOSE)
+class FilterMessages:
 
     def __init__(self,
                  me: Optional[str] = PurePath(__file__).stem,
@@ -67,7 +39,7 @@ class FilterMessages:
             )
             parser.add_argument(
                 '-b', '--boring-regex',
-                default=self.DEFAULT_THINGS_CONSIDERED_BORING.pattern,
+                default=DEFAULT_THINGS_CONSIDERED_BORING.pattern,
                 help='''
                     what to omit; the default is so complex, it cannot be printed here :-|
                     ''',
