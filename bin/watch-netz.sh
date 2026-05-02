554b7a78f86f15eb10fbc21126998b485100b488
Ann T Ropea <bedhanger@gmx.de>
2026-05-02 17:57:02 +0200

    watch-netz.sh: simplify and show all rts
    Signed-off-by: Ann T Ropea <bedhanger@gmx.de>

--- bin/watch-netz.sh
+++ bin/watch-netz.sh
@@ -2,16 +2,15 @@
 
 # Watch network parameters
 
-TERM=vt100 \
-	e \
-		watch \
-			--interval 3 \
-			--no-title \
-			--differences \
-				"\
-					parallel --jobs=0 --ungroup ::: \
-						\"ip -r route show\" \
-						\"64 2> /dev/null | \
-							grep ESTAB | grep -v localhost\" \
-						\"ip -s -s -d -h addr show enp5s0\" \
-				"
+e \
+    watch \
+	--interval=3 \
+	--no-title \
+	--no-wrap \
+	--color \
+	--differences \
+	    "parallel --jobs=0 --ungroup ::: \
+		\"ip -color=always -r -s -s -d route show tab all\" \
+		\"64 2> /dev/null | rg ESTAB | rg -v localhost\" \
+		\"ip -color=always -s -s -d -h addr show enp5s0\" \
+    "
